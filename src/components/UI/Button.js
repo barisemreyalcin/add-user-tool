cf037dcfc727cae23de589c69386096dfe06e3e6
Barış Emre Yalçın <barisemre.bey@gmail.com>
2023-09-21 14:52:37 +0300

    display users or error if it exists on the screen

---
+++ src/components/UI/Button.js
@@ -0,0 +1,16 @@
+import React from "react";
+import styles from "./Button.module.css";
+
+function Button(props) {
+    return (
+        <button 
+            className={styles.button}
+            onClick={props.onClick} 
+            type={props.type}
+        >
+            {props.children}
+        </button>
+    )
+}
+
+export default Button;
